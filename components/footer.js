4661f14fea6c38645a732a020345430701fa82d0
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-03-08 15:31:21 +0700

    modify: component and tw.css

--- components/footer.js
+++ components/footer.js
@@ -1,62 +1,62 @@
 class Footer extends HTMLElement {
-	constructor() {
-		super();
-	}
+    constructor() {
+        super();
+    }
 
-	connectedCallback() {
-		this.innerHTML = `
-        <footer
-          class="flex flex-col gap-24 bg-shades-700 px-24 py-12 text-shades-300 xl:px-40"
+    connectedCallback() {
+        this.innerHTML = `
+    <footer
+    class="flex flex-col gap-16 bg-shades-700 md:px-24 md:pt-12 pt-8 pb-4 px-7 text-shades-300 xl:px-40"
+  >
+    <div class="flex justify-between lg:gap-40 xl:gap-60 flex-col md:flex-row">
+      <div class="flex flex-col gap-4">
+        <span class="font-lora text-base font-bold text-shades-100"
+          >Berkain</span
         >
-          <div class="flex justify-between lg:gap-40 xl:gap-60">
-            <div class="flex flex-col gap-4">
-              <span class="font-lora text-base font-bold text-shades-100"
-                >Berkain</span
-              >
-              <div class="flex gap-16 lg:text-xs xl:text-sm">
-                <p>
-                  Berkain adalah merek kain asli Indonesia yang menyediakan berbagai
-                  jenis kain tradisional, seperti batik, tenun, songket, dan masih
-                  banyak lagi. Kami berkomitmen untuk mempromosikan kekayaan budaya
-                  Indonesia melalui karya-karya seni tekstil yang unik dan menarik.
-                </p>
-                <p>
-                  Kami percaya bahwa Berkain dapat menjadi pilihan tepat untuk Anda
-                  yang ingin tampil beda dan eksklusif, serta peduli dengan
-                  perkembangan industri kerajinan lokal di Indonesia. Selamat datang
-                  di Berkain, di mana keindahan kain Indonesia menjadi inspirasi
-                  bagi karya kami.
-                </p>
-              </div>
-            </div>
-            <div class="flex gap-14">
-              <div class="flex flex-col gap-2 text-base font-bold">
-                <span class="font-lora text-shades-100">Navigasi</span>
-                <ul class="flex flex-col gap-2 lg:text-xs xl:text-sm">
-                  <li>Beranda</li>
-                  <li>Telusuri</li>
-                  <li>Login</li>
-                  <li>Tentang Kami</li>
-                </ul>
-              </div>
-              <div class="flex flex-col gap-2 text-base font-bold">
-                <div class="w-max font-lora text-shades-100">Hubungi Kami</div>
-                <ul class="flex flex-col gap-2 lg:text-xs xl:text-sm">
-                  <li>Hotline</li>
-                  <li>Email</li>
-                  <li>Instagram</li>
-                  <li>Twitter</li>
-                  <li>Facebook</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="text-center font-lora text-base text-shades-100">
-            Hak Cipta © 2023 Berkain
-          </div>
-        </footer>
+        <div class="flex md:gap-16 gap-4 lg:text-xs xl:text-sm flex-col md:flex-row">
+          <p>
+            Berkain adalah merek kain asli Indonesia yang menyediakan berbagai
+            jenis kain tradisional, seperti batik, tenun, songket, dan masih
+            banyak lagi. Kami berkomitmen untuk mempromosikan kekayaan budaya
+            Indonesia melalui karya-karya seni tekstil yang unik dan menarik.
+          </p>
+          <p>
+            Kami percaya bahwa Berkain dapat menjadi pilihan tepat untuk Anda
+            yang ingin tampil beda dan eksklusif, serta peduli dengan
+            perkembangan industri kerajinan lokal di Indonesia. Selamat datang
+            di Berkain, di mana keindahan kain Indonesia menjadi inspirasi
+            bagi karya kami.
+          </p>
+        </div>
+      </div>
+      <div class="flex gap-16 md:mt-0 mt-12">
+        <div class="flex flex-col gap-2 text-base font-bold">
+          <span class="font-lora text-shades-100">Navigasi</span>
+          <ul class="flex flex-col gap-2 lg:text-xs xl:text-sm">
+            <li>Beranda</li>
+            <li>Telusuri</li>
+            <li>Login</li>
+            <li>Tentang Kami</li>
+          </ul>
+        </div>
+        <div class="flex flex-col gap-2 text-base font-bold">
+          <div class="w-max font-lora text-shades-100">Hubungi Kami</div>
+          <ul class="flex flex-col gap-2 lg:text-xs xl:text-sm">
+            <li>Hotline</li>
+            <li>Email</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+            <li>Facebook</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="text-center font-lora text-base text-shades-100 tracking-tighter">
+      Hak Cipta © 2023 Berkain
+    </div>
+  </footer>
       `;
-	}
+    }
 }
 
-customElements.define('footer-component', Footer);
+customElements.define("footer-component", Footer);
--- components/navbar.js
+++ components/navbar.js
@@ -1,17 +1,17 @@
 class Navbar extends HTMLElement {
-	constructor() {
-		super();
-	}
+    constructor() {
+        super();
+    }
 
-	connectedCallback() {
-		this.innerHTML = `
+    connectedCallback() {
+        this.innerHTML = `
           <nav class="flex w-full justify-between px-12 py-9">
             <i class="fa-solid fa-bars text-xl text-secondary"></i>
-            <h1 class="text-2xl font-semibold text-secondary">Berkain</h1>
+            <h1 class="text-2xl font-semibold text-secondary font-lora tracker-tighter drop-shadow-lg">Berkain</h1>
             <i class="fa-solid fa-cart-shopping text-xl text-secondary"></i>
           </nav>
       `;
-	}
+    }
 }
 
-customElements.define('navbar-component', Navbar);
+customElements.define("navbar-component", Navbar);
