f2e7f7ca34fd0b24c5814c55e4800aa0c1fe1a99
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-03-08 17:04:57 +0700

    feat: add checkout page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.{html,js}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				lora: ['Lora'],
-				nunito: ['Nunito'],
-			},
-			colors: {
-				primary: '#A39886',
-				secondary: '#FFF',
-				shades: {
-					100: '#FFFCE3',
-					300: '#D2BFA6',
-					400: '#6D614F',
-					500: '#40392C',
-					700: '#23201B',
-				},
-				'c-blue': {
-					500: '#324448',
-				},
-			},
-		},
-	},
-	plugins: [],
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                lora: ["Lora"],
+                nunito: ["Nunito"],
+            },
+            colors: {
+                primary: "#A39886",
+                secondary: "#FFF",
+                shades: {
+                    100: "#FFFCE3",
+                    300: "#D2BFA6",
+                    400: "#6D614F",
+                    500: "#40392C",
+                    700: "#23201B",
+                },
+                "c-blue": {
+                    500: "#324448",
+                },
+                "c-red": {
+                    500: "#4A0015",
+                },
+            },
+        },
+    },
+    plugins: [],
 };
